498c3fa7f1cf4c69beb95ea4207d1586ae8b2e98
mdaushi <firdaus.ex19@gmail.com>
2023-04-13 05:06:52 +0700

    init

---
+++ components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fixed top-0 z-50 w-full">
+    <div as="nav" class="bg-white dark:bg-black">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex w-full items-center justify-between">
+            <div class="flex flex-shrink-0 items-center">
+              <img
+                alt="Logo"
+                loading="lazy"
+                width="32"
+                height="32"
+                decoding="async"
+                data-nimg="1"
+                class="rounded-lg"
+                style="color: transparent"
+                src="/plugins-run.svg"
+              />
+            </div>
+            <!-- nav menu -->
+            <div></div>
+
+            <div>
+              <MoreNav></MoreNav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/AppHero.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1
+    class="flex w-full justify-center pt-12 text-5xl font-black text-neutral-800 dark:text-neutral-200"
+  >
+    IPNU<span class="text-neutral-400 dark:text-neutral-600">.</span>SPACE
+  </h1>
+  <div class="mx-auto max-w-3xl">
+    <div class="flex w-full justify-center py-6 px-4 sm:px-6 lg:px-8">
+      <h2
+        class="text-left text-xl tracking-wide text-neutral-700 dark:text-neutral-300 md:text-center"
+      >
+        Banyak hal menarik yang ada IPNU, mari saling kenal, saling beri impect.
+        sebaran hal positif dari rekan-rekan untuk semesta raya
+      </h2>
+    </div>
+  </div>
+</template>
