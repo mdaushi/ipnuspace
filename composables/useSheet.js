2d1d6574e4e8244d5c99aab25083138663930d3d
mdaushi <firdaus.ex19@gmail.com>
2023-04-13 10:50:30 +0700

    add function gsheet

---
+++ composables/useSheet.js
@@ -0,0 +1,23 @@
+let sheetRange = "Form Responses 1!A1:G200";
+
+const getVars = () => {
+  const SPREAD_SHEET_ID = useRuntimeConfig().public.SPREAD_SHEET_ID;
+  const GOOGLE_API_KEY = useRuntimeConfig().public.GOOGLE_API_KEY;
+
+  return { SPREAD_SHEET_ID, GOOGLE_API_KEY };
+};
+
+export async function allRows() {
+  const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${sheetRange}?key=${GOOGLE_API_KEY}`;
+  return await useFetch(url);
+}
+
+export async function singleRow(row) {
+  const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
+
+  const rowRange = `Sheet1!A${row}:D${row}`;
+
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${rowRange}?key=${GOOGLE_API_KEY}`;
+  return await useFetch(url);
+}
