b2e5f777bd52c5507bd0b19a06c224b0c2041f87
mdaushi <firdaus.ex19@gmail.com>
2023-04-16 15:10:57 +0700

    update meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["nuxt-headlessui", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  app: {
+    head: {
+      title: "IPNU SPACE",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/ipnuspacelogo.svg" }],
+      meta: [
+        {
+          name: "description",
+          content:
+            "ada banyak hal yang manarik tentang Pelajar Nahdlatul Ulama yang tersebar disemua sosial media. semua informasi organisasi, usaha, komunitas pengembangan bakat yang berkaitan tentang Pelajar Nahdlatul Ulama bisa terhimpun diaplikasi ini.",
+        },
+      ],
+    },
+  },
   runtimeConfig: {
     public: {
       GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
