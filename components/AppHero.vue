a3ad4274c5dbb4033cd22fac5a905fb09045eaa5
mdaushi <firdaus.ex19@gmail.com>
2023-04-16 13:17:04 +0700

    AppHero reusbale

--- components/AppHero.vue
+++ components/AppHero.vue
@@ -2,16 +2,25 @@
   <h1
     class="flex w-full justify-center pt-12 text-5xl font-black text-neutral-800 dark:text-neutral-200"
   >
-    IPNU<span class="text-neutral-400 dark:text-neutral-600">.</span>SPACE
+    {{ beforeTitle }} IPNU<span class="text-neutral-400 dark:text-neutral-600"
+      >.</span
+    >SPACE {{ afterTitle }}
   </h1>
   <div class="mx-auto max-w-3xl">
     <div class="flex w-full justify-center py-6 px-4 sm:px-6 lg:px-8">
       <h2
         class="text-left text-xl tracking-wide text-neutral-700 dark:text-neutral-300 md:text-center"
       >
-        Banyak hal menarik yang ada IPNU, mari saling kenal, saling beri impect.
-        sebaran hal positif dari rekan-rekan untuk semesta raya
+        <span v-html="description"></span>
       </h2>
     </div>
   </div>
 </template>
+
+<script setup>
+const props = defineProps({
+  description: String,
+  beforeTitle: String,
+  afterTitle: String,
+});
+</script>
